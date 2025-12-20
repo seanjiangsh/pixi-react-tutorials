eb906d7f7a420acc565c3ea7b1629369ec06b04d
Sean Jiang <seanjiangsh@gmail.com>
2025-12-21 07:57:24 +0800

    Changed to HashRouter to prevent gh-page 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "src/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/pixi-react-tutorials">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
