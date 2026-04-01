0b266c2d8050844a158d9ea7f0e5be47aa4d3bcc
Sean Jiang <seanjiangsh@gmail.com>
2026-04-01 09:08:15 +0800

    Updates

---
+++ storybook/assets/HelloWorld.stories-BseK51J0.js
@@ -0,0 +1,3 @@
+import{P as a,H as s}from"./HelloWorld-C2N9Kb9R.js";import"./index-gqP8PP-3.js";import"./iframe-Br4qoqJA.js";import"./preload-helper-Dp1pzeXC.js";import"./client-BJRNWXI_.js";const i={title:"Scenes/HelloWorld",component:s,parameters:{layout:"fullscreen"},decorators:[a]},r={args:{}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {}
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Default"];export{r as Default,d as __namedExportsOrder,i as default};
---
+++ storybook/assets/formatter-QJ4M4OGQ-CLv53c9z.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-DJlhl4--.js";import{b as t}from"./iframe-Br4qoqJA.js";import"./preload-helper-Dp1pzeXC.js";import"./react-18-DG-XZZzQ.js";import"./client-BJRNWXI_.js";import"./index-gqP8PP-3.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
